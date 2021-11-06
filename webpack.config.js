3467a5d9e5b44e65446a1e38d56148191c13f0c9
yzhbankov <yzhbankov@redpointpositioning.com>
2021-11-06 16:42:24 +0000

    make webpack building project

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   mode: process.env.NODE_ENV,
-  entry: path.resolve(__dirname, "./src/index.tsx"),
+  entry: path.resolve(__dirname, './src/index.tsx'),
   module: {
     rules: [
       {
@@ -31,27 +31,41 @@ module.exports = {
     extensions: ['.js', '.jsx', '.mjs', '.tsx', '.ts'],
   },
   devServer: {
-    publicPath: 'http://127.0.0.1:8086/',
-    contentBase: './',
+    // publicPath: 'http://127.0.0.1:8086/',
+    // contentBase: './',
     hot: true,
     headers: {'Access-Control-Allow-Origin': '*'},
     historyApiFallback: true,
-    disableHostCheck: true,
+    // disableHostCheck: true,
   },
   output: {
-    globalObject: 'this',
     path: path.resolve(__dirname, 'build'),
     filename: 'client.min.js',
     publicPath: '/',
-    chunkFilename: '[name].chunk.js',
+    chunkFilename: '[id].[chunkhash].js',
   },
   optimization: {
     splitChunks: {
-      chunks: 'initial',
+      chunks: 'async',
+      minSize: 20000,
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
     },
-  },
-  node: {
-    fs: "empty"
   },
   externals: [
     {
